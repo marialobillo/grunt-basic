8528b749d14cca1df1a987192ccfbcb4901fd5d0
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-07-27 18:07:40 +0200

    suma

--- gruntfile.js
+++ gruntfile.js
@@ -1,9 +1,13 @@
 module.exports = function (grunt){
   grunt.initConfig({
     uglify: {
+      suma: {
+        src: 'js/suma.js',
+        dest: 'dist/suma.min.js'
+      },
       dist: {
-        src: 'js/*.js'
-        dest: 'dist/operaciones.min.js'
+        src: 'js/*.js',
+        dest: 'dist/all.min.js'
       }
     }
   });
